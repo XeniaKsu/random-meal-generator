09451009cddf5567833be468168e62e45e295b8a
Ksenia Koneva <hello@xeniakoneva.co.uk>
2023-04-03 18:03:04 +0100

    adds button

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="max-x-4xl mx-auto px-4 py-10">
+      <button
+        className="bg-gray-800 text-white px-4 py-2 w-full rounded-md: w-40">
+       "Get new meal" </button>
+       <h1
+        className="text-4xl font-bold mt-6 underline">
+        Title</h1>
+        <div>
+          <div className="mt-4 border-orange">
+
+          </div>
+        </div>
     </div>
   )
 }
